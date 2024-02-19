a5ced699cc645b0fb396015ee2fd817a01d6c5d0
Yx Jiang <2237303+yxjiang@users.noreply.github.com>
2024-02-19 19:50:21 +0000

    Enable command dispatch

--- mnlm/robot/robot_arm_ws/src/robot_arm/robot_arm/command_dispatcher.py
+++ mnlm/robot/robot_arm_ws/src/robot_arm/robot_arm/command_dispatcher.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String, Float64
+from typing import Any, List
 import json
 
 
@@ -8,6 +9,7 @@ class CommandDispatcherNode(Node):
     def __init__(self):
         super().__init__("command_dispatcher_node")
         self.node_name = self.get_name()
+        self.num_joints = 5
         self.subscription = self.create_subscription(
             String, "json_command_topic", self._json_command_callback, 10
         )
@@ -15,37 +17,57 @@ class CommandDispatcherNode(Node):
         self.servo0_pub = self.create_publisher(Float64, "/servo0/control", 10)
 
     def _json_command_callback(self, msg):
+        """This function is called every time a message is received on the json_command_topic.
+        An example input is:
+        {
+            "operations": [
+                {
+                    "operation": "move_single_servo",
+                    "parameters": {"id": 1, "angle": 60, "time": 500}
+                },
+                {
+                    "operation": "set_rgb_light",
+                    "parameters": {"R": 255, "G": 0, "B": 0}
+                },
+                {
+                    "operation": "move_single_servo",
+                    "parameters": {"id": 1, "angle": 90, "time": 500}
+                }
+            ]
+        }
         """
-        This function is called every time a message is received on the json_command_topic
-        ."""
         try:
+            self.get_logger().info(f"Dispatching command: {msg.data}")
             command_data = json.loads(msg.data)
-            # Process the command_data to control the joint
-            self._process_command(command_data)
+            if "operations" in command_data:
+                # Process the command_data to control the joint
+                self._process_command(command_data["operations"])
         except json.JSONDecodeError:
             self.get_logger().error("Invalid JSON received")
 
-    def _process_command(self, command_data):
+    def _process_command(self, operations: List[Any]):
+        """Parse the incoming command and take the appropriate action.
+        """
         # Example: command_data = {"action": "move", "joint": "joint1", "value": 1.0}
-        action = command_data.get("action")
-        joint_name = command_data.get("joint")
-        value = command_data.get("value")
+        for operation in operations:
+            func_name = operation.get("operation", "")
+            parameters: Dict[str, Any] = operation.get("parameters", {})
 
-        if action == "move":
-            # Implement the logic to move the specified joint to the given value
-            # This will depend on your specific robot and control system
-            self._move_joint(joint_name, value)
-        else:
-            self.get_logger().warn(f"Unknown action: {action}")
+            if func_name == "move_single_servo":
+                self._move_single_joint(parameters.get("id", 0), parameters.get("angle", 0), parameters.get("time", 0))
+            elif func_name == "set_rgb_light":  # set the RGB light
+                pass
+            else:
+                self.get_logger().warn(f"Unknown action: {operation}")
 
-    def _move_joint(self, joint_name, value):
-        if joint_name == "servo0":
+    def _move_single_joint(self, id: int, angle: float, time: int):
+        if 0 <= id <= self.num_joints:
             control_msg = Float64()
-            control_msg.data = value
-            self.servo0_pub.publish(control_msg)
-            self.get_logger().info(f"{self.node_name} Moving {joint_name} to {value}")
+            # control_msg.data = value
+            # self.servo0_pub.publish(control_msg)
+            self.get_logger().error(f"{self.node_name} Moving {id} to {angle}")
         else:
-            self.get_logger().warn(f"{self.node_name} Unknown joint: {joint_name}")
+            self.get_logger().warn(f"{self.node_name} Unknown joint: {id}")
 
 
 def main(args=None):
